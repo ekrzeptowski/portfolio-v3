60c63aae1e38c3097a6e9c9e366bc83997c6c43a
ekrzeptowski <contact@ekrzeptowski.pl>
2023-08-25 19:23:41 +0200

    Add image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
-import {defineConfig} from "astro/config";
+import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: vercel(),
-    output: "hybrid",
-    integrations: [astroI18next(), tailwind(), react(), mdx()],
-    site: "https://ekrzeptowski.pl",
-    vite: {
-        ssr: {
-            noExternal: ["react-icons"]
-        }
-    }
+  adapter: vercel(),
+  output: "hybrid",
+  integrations: [astroI18next(), tailwind(), react(), mdx(), astroImageTools],
+  site: "https://ekrzeptowski.pl",
+  vite: {
+    ssr: {
+      noExternal: ["react-icons"],
+    },
+  },
 });
