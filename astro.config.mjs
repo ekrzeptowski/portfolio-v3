ae00424e1c155f941ef49bd20e6f7f4f7b0605c8
ekrzeptowski <contact@ekrzeptowski.pl>
2023-08-28 13:07:21 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,33 @@ import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
 import { astroImageTools } from "astro-imagetools";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
   output: "hybrid",
-  integrations: [astroI18next(), tailwind(), react(), mdx(), astroImageTools],
+  integrations: [
+    astroI18next(),
+    tailwind(),
+    react(),
+    mdx(),
+    astroImageTools,
+    sitemap({
+      i18n: {
+        defaultLocale: "pl",
+        locales: {
+          pl: "pl-PL",
+          en: "en-US",
+        },
+      },
+    }),
+    robotsTxt(),
+  ],
   site: "https://ekrzeptowski.pl",
   vite: {
     ssr: {
